c6e34b121a6d3f0d924be026a980ff5067efddfe
Kumar Saksham <121075015+KrSaksh@users.noreply.github.com>
2024-07-13 03:47:52 +0530

    Time: 0 ms (100%), Space: 10.6 MB (8.68%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -41,14 +41,6 @@ public:
                 grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
             }
         }
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                cout << grid[i][j] << " ";
-            }
-            cout << "\n";
-        }
         return grid[n - 1][m - 1];
     }
 };
