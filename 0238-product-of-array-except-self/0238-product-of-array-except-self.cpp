494fca2a94db3a789c04a41f150f759de0d458c0
Kumar Saksham <121075015+KrSaksh@users.noreply.github.com>
2025-06-18 16:26:23 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -13,26 +13,20 @@ public:
                 zero++;
             }
         }
-        vector<int> ans(nums.size(), 0);
-        if(zero > 1) return ans;
+        if(zero) {
+            vector<int> ans(nums.size(), 0);
+            if(zero > 1)
+                return ans;
+            else {
+                ans[idx] = x;
+                return ans;
+            }
+        }
         vector<int> prod(nums.size(), x);
-        // cout << "prod\n";
-        // for(auto &i : prod) {
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-        // cout << "nums\n";
-        // for(auto &i : nums) {
-        //     cout << i << " ";
-        // }
         for(int i = 0; i < nums.size(); i++)
         {
             if(nums[i] && !zero)
                 prod[i] /= nums[i];
-            else if(zero){
-                ans[idx] = x;
-                return ans;
-            }
         }
         return prod;
     }
